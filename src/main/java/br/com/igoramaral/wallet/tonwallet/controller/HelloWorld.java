76cd020a2f98908e421422a1ec86be9712c4fbb0
Igor Amaral <igordominices@poli.ufrj.br>
2020-02-18 16:08:50 -0300

    database test

--- src/main/java/br/com/igoramaral/wallet/tonwallet/controller/HelloWorld.java
+++ src/main/java/br/com/igoramaral/wallet/tonwallet/controller/HelloWorld.java
@@ -1,6 +1,11 @@
 
 package br.com.igoramaral.wallet.tonwallet.controller;
 
+import br.com.igoramaral.wallet.tonwallet.models.User;
+import br.com.igoramaral.wallet.tonwallet.repository.UserRepository;
+import java.util.List;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +16,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorld {
     
+    private final UserRepository userRepository;
+
+    public HelloWorld(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    
+    
+    
     @GetMapping("/")
     String hello() {
         return "Hello World from heroku!";
     }
+    
+    @GetMapping("/users")
+    List<User> users(){
+        return userRepository.findAll();
+    }
+    
+    @Bean
+    ApplicationRunner applicationRunner(UserRepository userRepository) {
+        return args -> {
+            userRepository.save(new User("Igor Amaral"));
+            userRepository.save(new User("Natalia Nascimento"));
+        };
+            
+    }
 }
+
+
---
+++ src/main/java/br/com/igoramaral/wallet/tonwallet/models/User.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.igoramaral.wallet.tonwallet.models;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+/**
+ *
+ * @author IgorAmaral
+ */
+@Entity
+@Table(name="tb_user")
+public class User{
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    @Column
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
