639e0dfebc7ef08453fc4bdf9f740ad21e0a2127
Igor Amaral <igordominices@poli.ufrj.br>
2020-02-18 14:30:51 -0300

    project creation

---
+++ src/main/java/br/com/igoramaral/wallet/tonwallet/TonwalletApplication.java
@@ -0,0 +1,13 @@
+package br.com.igoramaral.wallet.tonwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonwalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/igoramaral/wallet/tonwallet/TonwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igoramaral.wallet.tonwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TonwalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
