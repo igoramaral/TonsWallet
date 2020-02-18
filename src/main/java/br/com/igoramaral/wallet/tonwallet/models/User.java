262b75c4689fbd99e7996680d12376aa48ddc81e
Igor Amaral <igordominices@poli.ufrj.br>
2020-02-18 17:35:28 -0300

    added table name on user Entity

--- src/main/java/br/com/igoramaral/wallet/tonwallet/models/User.java
+++ src/main/java/br/com/igoramaral/wallet/tonwallet/models/User.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 
 /**
@@ -18,6 +19,7 @@ import javax.persistence.Id;
  */
 
 @Entity
+@Table(name="User")
 public class User{
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
